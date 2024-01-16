c209470cdefc25421c722d4d06d5065d2325cf4e
FernadaArrieta <arrietafernanda6@gmail.com>
2024-01-15 23:45:22 -0300

    first commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pokemon-container">
+    <img :src="imgSrc" class="hidden-pokemon" alt="pokemon" />
+    <img :src="imgSrc" v-if="showPokemon " class="fade-in" alt="pokemon" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      require: true,
+    },
+    showPokemon:{
+        type: Boolean,
+        require: true,
+        default: false
+    }
+
+  },
+
+  computed: {
+    imgSrc() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>¿Quien es este pokémon?</h1>
+  <PokemonOption/>
+  <PokemonPicture :pokemonId="4" :showPokemon ="false" />
+</template>
+
+<script>
+import PokemonOption from '@/components/PokemonOption.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue';
+import getPokemonsOptions from '@/helpers/getPokemonOptions'
+
+
+export default {
+  components:{
+    PokemonOption,
+    PokemonPicture
+
+  }
+
+}
+</script>
+
